02da5bf9e53b8919f05958359116dbb83d20ee5d
Manuel <robinard.manuel@gmail.com>
2020-10-26 11:14:04 +0100

    forgot to add bedore push..

---
+++ src/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+
+
+
+function Card ()  {
+
+    return (
+    
+    <div className = "card">
+        <div>
+        <img className = "cover" src="https://s31242.pcdn.co/wp-content/uploads/2020/10/ElBZ9tSXEAA4eOt-scaled-e1603465205747-1024x693.jpeg" alt ="ArianaGrande"></img>
+
+        </div>
+        <h1>Ariana Grande - Positions</h1>
+        <div> 
+        <p>Rate this single</p>
+        <img className ="stars" src="https://allstarmartialartswellington.com/wp-content/uploads/2019/11/evaluation-five-star-rating-favorite-like-recommend-icon-of-star-rating-png-256_256.png" alt="stars racing"></img>
+        <p className ="description">New single called "Positions" from her new Album position expected  on the 30th of October </p>
+
+        
+        </div>
+
+
+    </div>
+    
+
+
+
+
+
+    )}
+
+export default Card; 
